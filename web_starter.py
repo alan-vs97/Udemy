f1fc0a605e04baff1f7dab409c4045f5eb9a0951
alan-vs97 <alan.vilchis@hotmail.com>
2019-09-07 11:38:01 -0500

    FInished exercise

--- web_starter.py
+++ web_starter.py
@@ -19,3 +19,16 @@ with open(udemy_csv, "r", encoding="UTF-8") as csv_file:
     for row in csv_reader:
         title.append(row[1])
         price.append(row[4])
+        subscribers.append(row[5])
+        reviews.append(row[6])
+        reviews_percent.append(round(int(row[6])/int(row[5]), 2))
+        length.append(float(row[9].split(" ")[0]))
+
+cleanCsv = zip(title,price,subscribers,reviews,reviews_percent,length)
+
+output_file = os.path.join("webFinal.csv")
+
+with open(output_file, "w", newline='') as data_file:
+    writer = csv.writer(data_file)
+    writer.writerow(["Title", "Course Price", "Subscribers", "Reviews", "Percent of Reviews", "Length of Course"])
+    writer.writerows(cleanCsv)
